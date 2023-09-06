9ae9170563bc1b99df95735f72073606d4ccfddd
Nikita <audamoon@gmail.com>
2023-09-06 13:43:21 +0300

    finish humburger
    also change logo metrika

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function openBurgerMenu() {
+    var burger = document.querySelector('.burger__item');
+    var modal = document.querySelector('.menu-modal');
+    var menu = document.querySelector('.menu-container')
+    var navbar = document.querySelector('.navbar__inner')
+    burger.classList.toggle("open")
+    modal.appendChild(menu)
+    modal.classList.toggle("visible")
+    if (!modal.classList.contains("visible")) {
+        navbar.appendChild(menu)
+    }
+}

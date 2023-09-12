9640cb8a30de0ec6a9f893b498561a55a5796de4
Nikita <audamoon@gmail.com>
2023-09-12 15:26:24 +0300

    upd adding to counter
    add back and ajax logic

--- src/webmaster.php
+++ src/webmaster.php
@@ -1,15 +1,37 @@
 <?php
-$data_json = file_get_contents("request.json");
+require 'counter.php';
+
+$OAuth_token = $_POST["token"];
+$counter_id = $_POST["counter"];
+if (empty($OAuth_token) || empty($counter_id)) {
+    exit("Empty fields");
+}
+
+$domens_to_add = explode("\n",$_POST["domens"]);
+$mirrors = get_mirrors($counter_id, $OAuth_token);
+
+if (empty($domens_to_add)) {
+    exit("Empty fields");
+}
+
+foreach ($domens_to_add as $domen) {
+    if (!in_array($domen, $mirrors)) { 
+        $mirrors[] = array("site" => $domen);
+    }
+};
+
+//  $mirrors[] = array("site" => str_replace('\r','',$domen));
+
+$data_json = array('counter' => array ('mirrors2'=>$mirrors, "mirrors_remove"=>0));
 
 $ch = curl_init();
 
-$counter_id = "66051736";
 $url = "https://api-metrika.yandex.net/management/v1/counter/" . $counter_id;
 
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: OAuth y0_AgAAAABD63-hAAppYAAAAADrbQODULGtsFMHQjSuq_chSpjfrrC1myk',"Content-Type: application/json"]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: OAuth ' . $OAuth_token ,"Content-Type: application/json"]);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($data_json));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $res = curl_exec($ch);
